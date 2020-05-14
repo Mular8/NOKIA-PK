7a20c171227b1e622cec0e769073b63b24e320b7
Patryk Margasinski <patrykmargasinski@gmail.com>
2020-05-14 12:36:12 +0200

    Merge

---
+++ UE/Application/Sms/Ports/IUserPort.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "IUeGui.hpp"
+
+namespace ue
+{
+
+class IUserEventsHandler
+{
+public:
+    virtual ~IUserEventsHandler() = default;
+};
+
+class IUserPort
+{
+public:
+    virtual ~IUserPort() = default;
+
+    virtual void showNotConnected() = 0;
+    virtual void showConnecting() = 0;
+    virtual void showConnected() = 0;
+    virtual void showReceivedSms() = 0;
+};
+
+}
