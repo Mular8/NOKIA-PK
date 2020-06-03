4c488f8b9da54696553b91c73c4d302feb86e4c1
dariusz202 <wasik202@gmail.com>
2020-06-03 23:31:05 +0200

    fix

--- UE/Application/States/TalkingState.hpp
+++ UE/Application/States/TalkingState.hpp
@@ -9,7 +9,6 @@ class TalkingState : public BaseState
 {
 public:
     TalkingState(Context& context, common::PhoneNumber from);
-    void handleSib(common::BtsId btsId) override;
 private:
     common::PhoneNumber from;
 };
